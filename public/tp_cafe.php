148060a5a39830d80d34917ba9fb5bea64ee7776
naimdevweb <naimhamidi19@gmal.com>
2025-01-09 18:31:58 +0100

    sucre et payer fini

--- public/tp_cafe.php
+++ public/tp_cafe.php
@@ -1,10 +1,14 @@
 <?php
 require_once("../utils/autoloader.php");
 $machine = new MachineACafe();
-echo $machine -> allumage();
-echo $machine -> mettreuneDosette();
-echo $machine -> faireDuCafe();
-echo $machine -> offMachine();
-echo $machine -> offMachine();
+echo $machine->allumage();
+echo $machine->allumage();
+echo $machine->setPrixCafe(2.50);
+echo $machine->setPrixSucre(2.50);
+echo $machine->insererArgent(5);
+echo $machine->mettreUneDosette();
+echo $machine->acheterCafe(); 
+
+ 
 
   
--- src/Entities/MachineACafe.php
+++ src/Entities/MachineACafe.php
@@ -1,53 +1,147 @@
 <?php
-class MachineACafe{
-private bool $cafe;
-private string $marque;
-private bool $enFonction;
- 
-public function __construct(string $marque = 'senseo<br> '){
-    $this->marque = $marque;
-    $this->cafe = false;
-    $this->enFonction = false;
-}
+class MachineACafe {
+    private bool $cafe;
+    private string $marque;
+    private bool $enFonction;
+    private bool $sucre;
+    private float $monnayeur; 
+    private float $prixCafe; 
+    private float $prixSucre; 
 
-public function allumage(){
-if($this->enFonction == true){
-    return "{$this->marque} la machine est deja allumer <br>";
-} else{
-$this->enFonction = true;
-return " {$this->marque}  la machine s'allume <br>";
-}
-}
-
-public function mettreuneDosette(){
-if($this->cafe == true ){
-    return 'il ya deja une dossete <br>';
-}else if ($this->cafe == false ){
-    $this->cafe = true;
-    return 'mettre une dossete <br>';
-}
-}
-
-public function faireDuCafe(){
-    if($this->enFonction == true && $this->cafe == true){
-        return 'Le cafe est pret <br>';
-    }else if ($this->enFonction == false){
-        return 'veuillez allumer la machine <br>';
-    }
-}
-
-public function offMachine(){
-    // Si la machine est éteinte
-    if($this->enFonction == false){
-
-        // On dit que la machine est déja éteinte
-        return 'la machine est deja eteint <br>';
-    // Sinon si la machine est allumée
-    }else if ($this->enFonction == true){
-        // Eteindre la machine
+    public function __construct(string $marque = 'senseo<br> ') {
+        $this->marque = $marque;
+        $this->cafe = false;
         $this->enFonction = false;
-        // On dit "allumez la machine
-        return 'On éteint la machine  <br>';
+        $this->sucre = false;
+        $this->monnayeur = 0.0;  
+        $this->prixCafe = 2.0;   
+        $this->prixSucre = 0.5;  
     }
-}
+
+    // Setter pour définir le prix du café
+    public function setPrixCafe(float $prix): void {
+        $this->prixCafe = $prix;
+    }
+
+    // Getter pour récupérer le prix du café
+    public function getPrixCafe(): float {
+        return $this->prixCafe;
+    }
+
+    // Setter pour définir le prix du sucre
+    public function setPrixSucre(float $prix): void {
+        $this->prixSucre = $prix;
+    }
+
+    // Getter pour récupérer le prix du sucre
+    public function getPrixSucre(): float {
+        return $this->prixSucre;
+    }
+
+    public function allumage() {
+        if ($this->enFonction == true) {
+            return " la machine est déjà allumée <br>";
+        } else {
+            $this->enFonction = true;
+            return "{$this->marque} la machine s'allume <br>";
+        }
+    }
+
+    public function mettreUneDosette() {
+        
+        $prixTotal = $this->prixCafe + $this->sucre ? $this->prixSucre : 0;
+
+        if ($this->monnayeur < $prixTotal) {
+            return "Vous n'avez pas assez d'argent pour ajouter une dosette. <br>";
+        }
+
+        if ($this->cafe == true) {
+            return 'Il y a déjà une dosette <br>';
+        } else {
+            $this->cafe = true;
+            return 'Mettre une dosette <br>';
+        }
+    }
+
+    public function faireDuCafe() {
+        
+        $prixTotal = $this->prixCafe + ($this->sucre ? $this->prixSucre : 0);
+
+        if ($this->monnayeur < $prixTotal) {
+            return "Vous n'avez pas assez d'argent pour faire du café. <br>";
+        }
+
+        if ($this->enFonction == true && $this->cafe == true) {
+            $sucreStatus = $this->sucre ? 'avec sucre' : 'sans sucre';
+            return "Le café est prêt, servi $sucreStatus <br>";
+        } else if ($this->enFonction == false) {
+            return 'Veuillez allumer la machine <br>';
+        }
+    }
+
+    public function offMachine() {
+        if ($this->enFonction == false) {
+            return 'La machine est déjà éteinte <br>';
+        } else {
+            $this->enFonction = false;
+            return 'On éteint la machine <br>';
+        }
+    }
+
+    public function ajouterSucre() {
+        if ($this->enFonction == false) {
+            return 'La machine est éteinte, vous ne pouvez pas ajouter de sucre. <br>';
+        }
+
+        if ($this->sucre == false) {
+            $this->sucre = true;
+            return 'Sucre ajouté au café <br>';
+        } else {
+            return 'Le café contient déjà du sucre <br>';
+        }
+    }
+
+    public function retirerSucre() {
+        if ($this->enFonction == false) {
+            return 'La machine est éteinte, vous ne pouvez pas retirer le sucre. <br>';
+        }
+
+        if ($this->sucre == true) {
+            $this->sucre = false;
+            return 'Sucre retiré du café <br>';
+        } else {
+            return 'Le café ne contient pas de sucre <br>';
+        }
+    }
+
+    public function verifierSucre() {
+        return $this->sucre ? 'Le café contient du sucre <br>' : 'Le café ne contient pas de sucre <br>';
+    }
+
+    // Méthode pour insérer de l'argent dans la machine
+    public function insererArgent(float $montant) {
+        if ($this->enFonction == false) {
+            return 'La machine est éteinte, vous ne pouvez pas insérer de l\'argent. <br>';
+        }
+        $this->monnayeur += $montant;
+        return "Vous avez inséré $montant € <br>";
+    }
+
+    // Méthode pour acheter un café avec ou sans sucre
+    public function acheterCafe() {
+        if ($this->enFonction == false) {
+            return 'La machine est éteinte, vous ne pouvez pas acheter de café. <br>';
+        }
+
+      
+        $prixTotal = $this->prixCafe + $this->sucre ? $this->prixSucre : 0;
+
+        if ($this->monnayeur < $prixTotal) {
+            return 'Vous n\'avez pas assez d\'argent pour acheter un café. <br>';
+        }
+
+        $montantRestant = $this->monnayeur - $prixTotal;
+        $this->monnayeur = 0;  
+        return "Café acheté avec succès. Monnaie rendue : $montantRestant € <br>";
+    }
 }
